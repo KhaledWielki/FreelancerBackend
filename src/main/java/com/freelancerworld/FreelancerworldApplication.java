253593c4d215c3c8267308b4d636112bda48a13c
Patron <patroneq@gmail.com>
2017-10-28 16:54:59 +0200

    Postawienie serwera

--- src/main/java/com/freelancerworld/FreelancerworldApplication.java
+++ src/main/java/com/freelancerworld/FreelancerworldApplication.java
@@ -1,12 +1,12 @@
-package com.freelancer.backend;
+package com.freelancerworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class FreelancerworldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(FreelancerworldApplication.class, args);
 	}
 }
---
+++ src/main/java/com/freelancerworld/service/UserService.java
@@ -0,0 +1,8 @@
+package com.freelancerworld.service;
+
+import com.freelancerworld.model.User;
+
+public interface UserService {
+	public User findUserByEmail(String email);
+	public void saveUser(User user);
+}
