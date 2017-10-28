cba361e0dea6cd3f3c1cf8ffc8e98ad26b1a8af4
KhaledWielki <adam.rezner95@gmail.com>
2017-10-28 12:12:51 +0200

    Implementacja WebMvcConfig

---
+++ src/main/java/com/freelancer/backend/configuration/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.freelancer.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by AdamR on 2017-10-28.
+ */
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+}
