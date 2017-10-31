4db0299bfda593571930563176ff2edb01aeba2e
adam <adam.rezner95@gmail.com>
2017-10-31 10:41:43 +0100

    Dodanie klasy Request

---
+++ src/main/java/com/freelancerworld/model/Request.java
@@ -0,0 +1,92 @@
+package com.freelancerworld.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by AdamR on 2017-10-31.
+ */
+
+@Entity
+@Table(name = "request")
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "request_id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "min_payment")
+    private int minPayment;
+
+    @Column(name = "max_payment")
+    private int maxPayment;
+
+    @Column(name = "deposit")
+    private int deposit;
+
+    @Column(name = "active")
+    private int active;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profession_id")
+    private Profession profession;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getMinPayment() {
+        return minPayment;
+    }
+
+    public void setMinPayment(int minPayment) {
+        this.minPayment = minPayment;
+    }
+
+    public int getMaxPayment() {
+        return maxPayment;
+    }
+
+    public void setMaxPayment(int maxPayment) {
+        this.maxPayment = maxPayment;
+    }
+
+    public int getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(int deposit) {
+        this.deposit = deposit;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+}
