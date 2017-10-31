4945b806a2f50e1992fbfadcf7475550459cf503
KhaledWielki <adam.rezner95@gmail.com>
2017-10-31 06:48:00 +0100

    Dodanie brakujących mutatorów

--- src/main/java/com/freelancerworld/model/OrderMaker.java
+++ src/main/java/com/freelancerworld/model/OrderMaker.java
@@ -21,4 +21,19 @@ public class OrderMaker {
     @JoinColumn(name = "user_id")
     private User user;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/freelancerworld/model/OrderTaker.java
+++ src/main/java/com/freelancerworld/model/OrderTaker.java
@@ -20,4 +20,20 @@ public class OrderTaker {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
