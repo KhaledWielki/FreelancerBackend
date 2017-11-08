37d9f00ef982dfde1c5444685b8ac1db5484f223
patroneq <patroneq@gmail.com>
2017-11-08 12:48:25 +0100

    Dodanie pliku .sql z kilkoma profesjami

---
+++ src/main/resources/static/db/insert_professions.sql
@@ -0,0 +1,6 @@
+INSERT INTO `profession` VALUES (1,'Stolarz');
+INSERT INTO `profession` VALUES (2,'Hydraulik');
+INSERT INTO `profession` VALUES (3,'Informatyk');
+INSERT INTO `profession` VALUES (4,'Kierowca');
+INSERT INTO `profession` VALUES (5,'Lekarz');
+INSERT INTO `profession` VALUES (6,'Opiekunka');
