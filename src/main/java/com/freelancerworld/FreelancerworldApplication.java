66d4da3edeb39ac7da9e97399803ff6e40421ef1
KhaledWielki <adam.rezner95@gmail.com>
2017-11-21 10:50:14 +0100

    Dodanie main.css, akutalizacja startu aplikacji

--- src/main/java/com/freelancerworld/FreelancerworldApplication.java
+++ src/main/java/com/freelancerworld/FreelancerworldApplication.java
@@ -1,10 +1,16 @@
 package com.freelancerworld;
 
+import com.freelancerworld.model.Address;
+import com.freelancerworld.service.Implementation.AddressServiceImpl;
+import com.freelancerworld.service.Implementation.ProfessionServiceImpl;
+import com.freelancerworld.service.Implementation.RequestServiceImpl;
+import com.freelancerworld.service.Implementation.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FreelancerworldApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FreelancerworldApplication.class, args);
