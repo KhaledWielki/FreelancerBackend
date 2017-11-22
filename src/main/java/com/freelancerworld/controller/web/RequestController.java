3a4cb802607cd1cb327e78253bf524ede11daf41
KhaledWielki <adam.rezner95@gmail.com>
2017-11-23 00:10:33 +0100

    Można mienić status requestu na webapce

--- src/main/java/com/freelancerworld/controller/web/RequestController.java
+++ src/main/java/com/freelancerworld/controller/web/RequestController.java
@@ -5,6 +5,7 @@ import com.freelancerworld.service.Implementation.RequestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,5 +23,18 @@ public class RequestController {
         List<Request> requestList = requestService.findAllRequests();
         model.addAttribute("requestslist", requestList);
         return "admin/requests";
+    }
+
+    @RequestMapping(value = "/admin/requests/changeactive/{id}")
+    public String deleteRequest(@PathVariable Long id) {
+        Request tempRequest = requestService.findRequestById(id);
+        if(tempRequest.getActive() == 0) {
+            tempRequest.setActive(1);
+        }
+        else {
+            tempRequest.setActive(0);
+        }
+        requestService.saveRequest(tempRequest);
+        return "redirect:/admin/requests";
     }
 }
