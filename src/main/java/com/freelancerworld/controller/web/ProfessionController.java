a4f0719875ea556cb7fbae668b6abdc5b98411e0
KhaledWielki <adam.rezner95@gmail.com>
2017-11-25 14:08:18 +0100

    Dodanie metod zwiazanych z edycją i usuwaniem profesji w kontrollerze

--- src/main/java/com/freelancerworld/controller/web/ProfessionController.java
+++ src/main/java/com/freelancerworld/controller/web/ProfessionController.java
@@ -5,6 +5,7 @@ import com.freelancerworld.service.Implementation.ProfessionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,5 +22,36 @@ public class ProfessionController {
         List<Profession> professionList= professionService.findAllProfessions();
         model.addAttribute("professionslist", professionList);
         return "admin/professions";
+    }
+
+    @RequestMapping(value = "/admin/profession/edit/{id}")
+    public String editProfession(@PathVariable int id, Model model) {
+        Profession tempProfession= professionService.findProfessionById(id);
+        model.addAttribute("profession", tempProfession);
+        return "admin/professionform";
+    }
+
+    @RequestMapping(value = "profession", method = RequestMethod.POST)
+    public String saveEditedProfession(Profession profession) {
+        professionService.saveProfession(profession);
+        return "redirect:/admin/profession/show/" + profession.getId();
+    }
+
+    @RequestMapping(value = "admin/profession/show/{id}")
+    public String showProfession(@PathVariable int id, Model model) {
+        Profession tempProfession = professionService.findProfessionById(id);
+        model.addAttribute("profession", tempProfession);
+        return "admin/professionshow";
+    }
+
+    @RequestMapping("admin/profession/new")
+    public String newProfession(Model model){
+        model.addAttribute("profession", new Profession());
+        return "admin/professionform";
+    }
+    @RequestMapping(value = "admin/profession/delete/{id}")
+    public String deleteProfession(@PathVariable int id) {
+        professionService.deleteProfessionById(id);
+        return "redirect:/admin/professions";
     }
 }
